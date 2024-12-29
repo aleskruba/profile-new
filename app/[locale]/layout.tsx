83a0601ded2ae278726d4d56c9bff80c03959757
Ales <aleskruba@seznam.cz>
2024-12-29 17:32:38 +0100

    dark mode default

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -33,7 +33,7 @@ export default async function LocaleLayout({
       <body suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >

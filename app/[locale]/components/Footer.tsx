f5f0a40980146de0bc8cd14c7f2b94214a6a8aaa
Ales <aleskruba@seznam.cz>
2024-12-31 17:43:53 +0100

    footern

--- app/[locale]/components/Footer.tsx
+++ app/[locale]/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="absolute b-0 w-full flex-col py-2 flex justify-center items-center dark:bg-gray-800 bg-gray-100">
+    <footer className="absolute b-0 w-full min-w-[99vw] flex-col py-2 flex justify-center items-center dark:bg-gray-800 bg-gray-100">
       <p className="text-sm">Created by <strong>Aleš Kruba</strong>, 2024</p>
       <p className="text-xs pt-2">All rights reserved... and I lost a few when I entered the married state.</p>
 

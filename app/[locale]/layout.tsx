30b02f382e55df0a264751a3c05d7e473b6cf290
Ales <aleskruba@seznam.cz>
2025-01-05 09:41:17 +0100

    add meta tags

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,8 @@ import { setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider"
+import Head from 'next/head';
+
 
 import { Poppins } from 'next/font/google'
 import ServerComponent from './components/Servercomponent';
@@ -39,6 +41,20 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <Head>
+  <title>Aleš Kruba - Full Stack Web Developer</title>
+  <meta name="description" content="Portfolio of Aleš Kruba, a full-stack web developer specializing in frontend and backend development using JavaScript, React, Next.js, and Express.js." />
+  <meta name="author" content="Aleš Kruba" />
+  <meta name="keywords" content="Portfolio, Full Stack Web Development, JavaScript, TypeScript, React js, Next js, Express js, Tailwind CSS, Material UI, Docker, Databases, PHP, Python" />
+  
+
+  <meta property="og:title" content="Aleš Kruba - Full Stack Web Developer Portfolio" />
+  <meta property="og:description" content="Explore the portfolio of Aleš Kruba, showcasing my skills in frontend and backend web development, including React, Next.js, Express.js, and more. Projects and experiences displayed in a dynamic, user-friendly format." />
+  <meta property="og:url" content="https://dokram.com" />
+  <meta property="og:image" content="https://dokram.com/man1.png" />
+  
+
+</Head>
       <body suppressHydrationWarning className={`text-center pb-4 ${poppins.className}`}>
         <ThemeProvider
           attribute="class"
